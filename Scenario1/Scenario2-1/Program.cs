4e8ce272b680d982f9b9c1885ecd76540761ee55
Stuxles <dennisboer@gmail.com>
2017-11-29 18:11:32 +0100

    Scenario2-1

---
+++ Scenario1/Scenario2-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            Console.Write("Enter name:");
+            try
+            {
+                p.Name = Console.ReadLine();
+                Console.WriteLine("The name is: {0}", p.Name);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae);
+            }
+        }
+    }
+}
