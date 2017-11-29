81c73a952eb7690b855dd7bac4ad0a922a06caf4
Stuxles <dennisboer@gmail.com>
2017-11-29 22:27:35 +0100

    Scenario2-4

---
+++ Scenario1/Scenario2-4/SampleClass.cs
@@ -0,0 +1,21 @@
+﻿namespace Scenario2_4
+{
+    public delegate void SampleDelegate();
+    public interface ISampleEvents
+    {
+        event SampleDelegate SampleEvent;
+        void Invoke();
+    }
+    public class SampleClass : ISampleEvents
+    {
+        public event SampleDelegate SampleEvent;
+
+        public void Invoke()
+        {
+            if (SampleEvent != null)
+            {
+                SampleEvent();
+            }
+        }
+    }
+}
