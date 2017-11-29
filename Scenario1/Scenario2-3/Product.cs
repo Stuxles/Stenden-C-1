8c3a5835bbf5a9fae7d6b399aa774c97e96ff56c
Stuxles <dennisboer@gmail.com>
2017-11-29 19:12:15 +0100

    Scenario2-3

---
+++ Scenario1/Scenario2-3/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Scenario2_3
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("{0:C}", price);
+            return "Product: " + name + " " + s;
+        }
+    }
+}
---
+++ Scenario1/Scenario2-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Price:");
+            double price = Double.Parse(Console.ReadLine());
+
+            Product p = new Product(name, price);
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
