2ec75ebfc71023cbcc2796f46008863210ba07ac
Stuxles <dennisboer@gmail.com>
2017-12-06 19:08:57 +0100

    week 3

--- Scenario1/Scenario3-3/Program.cs
+++ Scenario1/Scenario3-3/Program.cs
@@ -5,49 +5,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Scenario3_2
+namespace Scenario3_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Queue first = new Queue();
-            first.Enqueue(4);
-            first.Enqueue(20);
-            first.Enqueue(32);
-            first.Enqueue(45);
+            Stack first = new Stack();
+            first.Push(87);
+            first.Push(75);
+            first.Push(54);
+            first.Push(32);
+            first.Push(12);
 
-            Queue second = new Queue();
-            second.Enqueue(9);
-            second.Enqueue(14);
-            second.Enqueue(34);
-            second.Enqueue(66);
+            Stack second = new Stack();
+            second.Push(98);
+            second.Push(76);
+            second.Push(65);
+            second.Push(43);
+            second.Push(2);
 
-            ProcessQueue(first, second);
+            ProcessStack(first, second);
         }
 
-        static void ProcessQueue(Queue first, Queue second)
+        static void ProcessStack(Stack first, Stack second)
         {
             while (first.Count > 0 || second.Count > 0)
             {
                 if (first.Count == 0)
                 {
-                    Console.WriteLine(second.Dequeue());
+                    Console.WriteLine(second.Pop());
                     continue;
                 }
                 if (second.Count == 0)
                 {
-                    Console.WriteLine(first.Dequeue());
+                    Console.WriteLine(first.Pop());
                     continue;
                 }
 
                 if ((int)first.Peek() >= (int)second.Peek())
                 {
-                    Console.WriteLine(second.Dequeue());
+                    Console.WriteLine(second.Pop());
                 }
                 else
                 {
-                    Console.WriteLine(first.Dequeue());
+                    Console.WriteLine(first.Pop());
                 }
             }
         }
---
+++ Scenario1/Scenario3-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] words = {"bananen", "appels", "peren", "druiven", "komkommer"};
+            LinkedList<string> list = new LinkedList<string>(words);
+
+            Console.WriteLine("Product:");
+            String search = Console.ReadLine();
+            if (list.Contains(search))
+            {
+                Console.WriteLine("lijst bevat item");
+            }
+            else
+            {
+                Console.WriteLine("item niet gevonden");
+            }
+        }
+    }
+}
