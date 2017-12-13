e7398ac6fb207216c71849758cb0faedc8734f69
Stuxles <dennisboer@gmail.com>
2017-12-13 21:39:34 +0100

    4-4

---
+++ Scenario1/Scenario4-4/WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Scenario4_4
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string field1 = (string)(Session["Name"]);
+            string field2 = (string)(Session["Email"]);
+            if (field1 == String.Empty || field2 == String.Empty)
+            {
+                Response.Write("Fill in all fields");
+            }
+            else
+            {
+                Response.Write($"Name: {field1} <br>Email: {field2}");
+            }
+        }
+    }
+}
